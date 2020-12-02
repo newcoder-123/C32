3c5fea0921baba1c4df3a03283ede1b56e0ef28a
Selma <emmasmith1656@gmail.com>
2020-12-01 20:37:43 -0800

    2:30pm

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    //backgroundImg = loadImage("sprites/bg.png");
+    getTime();
 }
 
 function setup(){
@@ -45,18 +47,24 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
-    Engine.update(engine);
+    textSize(35)
+    fill("white")
+    text("score"+score,width-300,50)
+    Engine.update(engine);[]
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score()
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score()
     log3.display();
 
     box5.display();
@@ -85,4 +93,21 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getTime(){
+     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+     var responseJSON = await response.json();
+     //console.log(responseJSON.datetime)
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    console.log(hour)
+    if(hour>=06&&hour<=19){
+        bg = "sprites/bg2.jpg"
+    }
+    else{
+        bg = "sprites/bg.png"
+    }
+    backgroundImg = loadImage(bg)
+    console.log(backgroundImg)
 }
